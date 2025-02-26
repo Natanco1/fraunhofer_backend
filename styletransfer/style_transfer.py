386d228c19f884420bafd65a779794384a293d6d
Natanco1 <nata.oliveira2004@gmail.com>
2025-02-26 15:00:14 -0300

    feat: project kind of working

---
+++ styletransfer/style_transfer.py
@@ -0,0 +1,98 @@
+import torch
+from torch import nn
+from torchvision import models, transforms
+from PIL import Image
+import numpy as np
+import os
+
+
+class StyleTransfer:
+    def __init__(self, content_weight=1, style_weight=1000000, num_steps=500):
+        self.content_weight = content_weight
+        self.style_weight = style_weight
+        self.num_steps = num_steps
+
+        self.model = models.vgg19(pretrained=True).features.eval()
+
+    def preprocess_image(self, image_path):
+        """
+        Preprocess the image by resizing and normalizing it
+        """
+        transform = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+
+        image = Image.open(image_path)
+        image = transform(image).unsqueeze(0)
+        return image
+
+    def get_features(self, image):
+        """
+        Extract features from specific layers of VGG19
+        """
+        layers = {
+            '0': 'conv1_1',
+            '5': 'conv2_1',
+            '10': 'conv3_1',
+            '19': 'conv4_1',
+            '28': 'conv5_1'
+        }
+
+        features = {}
+        x = image
+        for name, layer in self.model._modules.items():
+            x = layer(x)
+            if name in layers:
+                features[layers[name]] = x
+        return features
+
+    def run_style_transfer(self, content_img, style_img):
+        """
+        Perform style transfer using content and style images
+        """
+        content_features = self.get_features(content_img)
+        style_features = self.get_features(style_img)
+
+        target = content_img.clone().requires_grad_(True)
+        optimizer = torch.optim.LBFGS([target])
+
+        for step in range(self.num_steps):
+            def closure():
+                target.data.clamp_(0, 1)
+                optimizer.zero_grad()
+
+                target_features = self.get_features(target)
+
+                content_loss = self.content_weight * torch.nn.functional.mse_loss(target_features['conv4_1'], content_features['conv4_1'])
+
+                style_loss = 0
+                for layer in style_features:
+                    target_feature = target_features[layer]
+                    style_feature = style_features[layer]
+                    _, c, h, w = target_feature.size()
+                    target_gram = target_feature.view(c, h * w).mm(target_feature.view(c, h * w).t())
+                    style_gram = style_feature.view(c, h * w).mm(style_feature.view(c, h * w).t())
+                    style_loss += torch.nn.functional.mse_loss(target_gram, style_gram) / (c * h * w)
+
+                loss = content_loss + self.style_weight * style_loss
+                loss.backward()
+
+                return loss
+
+            optimizer.step(closure)
+
+        return target
+
+    def save_image(self, tensor, filename="styled_image.jpg"):
+        """
+        Save the tensor as an image to disk
+        """
+        unloader = transforms.ToPILImage()
+        image = tensor.cpu().clone()
+        image = unloader(image.squeeze(0))
+        image.save(filename)
--- styletransfer/views.py
+++ styletransfer/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage, FileSystemStorage
+from .style_transfer import StyleTransfer
+import os
 
-# Create your views here.
+@csrf_exempt
+def style_transfer_view(request):
+    if request.method == "POST":
+        # Get the uploaded images
+        content_file = request.FILES['content_image']
+        style_file = request.FILES['style_image']
+
+        # Save the content and style images in the media directory
+        content_path = default_storage.save('content.jpg', content_file)
+        style_path = default_storage.save('style.jpg', style_file)
+
+        # Get the absolute path of the saved images
+        content_absolute_path = default_storage.path(content_path)  # Absolute path to content image
+        style_absolute_path = default_storage.path(style_path)  # Absolute path to style image
+
+        # Create an instance of the StyleTransfer class
+        style_transfer = StyleTransfer(content_weight=1, style_weight=1000000, num_steps=500)
+
+        # Preprocess the images (pass absolute paths)
+        content_img = style_transfer.preprocess_image(content_absolute_path)
+        style_img = style_transfer.preprocess_image(style_absolute_path)
+
+        # Run the style transfer model
+        output = style_transfer.run_style_transfer(content_img, style_img)
+
+        # Define the output image path (in the media folder)
+        fs = FileSystemStorage()
+        output_filename = fs.save('styled_image.jpg', output)  # Saving output image
+        output_url = fs.url(output_filename)  # Get the URL of the saved image
+
+        # Respond with the URL of the generated image
+        response = JsonResponse({
+            'message': 'Style Transfer Successful',
+            'image_url': output_url
+        })
+        return response
