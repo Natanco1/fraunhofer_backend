b359ddc08dfe13301a30bb13f95b76b14dd17976
Natanco1 <nata.oliveira2004@gmail.com>
2025-03-21 15:54:59 -0300

    feat: swap to cpu to get it to work

--- styletransfer/style_transfer.py
+++ styletransfer/style_transfer.py
@@ -1,106 +1,45 @@
 import tensorflow as tf
+import tensorflow_hub as hub
 import numpy as np
-from PIL import Image
-from tqdm import tqdm
+import PIL.Image
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 class StyleTransfer:
-    def __init__(self, content_weight=1e3, style_weight=1e-2, total_variation_weight=1e-6):
-        self.content_weight = content_weight
-        self.style_weight = style_weight
-        self.total_variation_weight = total_variation_weight
-        self.vgg_model = self._load_vgg_model()
+    def __init__(self, content_image_path, style_image_path, max_res=512):
+        self.content_image_path = content_image_path
+        self.style_image_path = style_image_path
+        self.max_res = max_res
+        self.model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
-    def _load_vgg_model(self):
-        """Load the VGG19 model for feature extraction."""
-        vgg19 = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
-        vgg19.trainable = False
-        return vgg19
+    def load_image(self, image_path):
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_image(image, channels=3)
+        image = tf.image.convert_image_dtype(image, tf.float32)
 
-    def _preprocess_image(self, image_path):
-        """Preprocess an image to match the input requirements of VGG19."""
-        image = Image.open(image_path)
-        image = image.resize((224, 224))
-        image = np.array(image) / 255.0
-        image = np.expand_dims(image, axis=0)
-        return tf.convert_to_tensor(image, dtype=tf.float32)
+        shape = tf.cast(tf.shape(image)[:-1], tf.float32)
+        long_side = max(shape)
+        scaling_factor = self.max_res / long_side
 
-    def _deprocess_image(self, image):
-        """Deprocess the image back to a viewable format."""
-        image = image.numpy().squeeze()
-        image = image * 255.0
-        image = np.clip(image, 0, 255).astype(np.uint8)
-        return Image.fromarray(image)
+        new_shape = tf.cast(shape * scaling_factor, tf.int32)
+        image = tf.image.resize(image, new_shape)
+        image = image[tf.newaxis, :]
 
-    def _get_content_and_style_features(self, content_image, style_image):
-        """Extract content and style features using the VGG19 model."""
-        content_layers = ['block5_conv2']
-        style_layers = [
-            'block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1'
-        ]
+        return image
 
-        model = tf.keras.models.Model(inputs=self.vgg_model.input, outputs=[self.vgg_model.get_layer(layer).output for layer in content_layers + style_layers])
+    def tensor_to_image(self, tensor):
+        tensor = np.array(tensor * 255, dtype=np.uint8)
 
-        content_features = model(content_image)[0]
-        style_features = model(style_image)
-        return content_features, style_features
+        if np.ndim(tensor) > 3:
+            tensor = tensor[0]
 
-    def _compute_content_loss(self, content_image, target_image):
-        """Compute the content loss."""
-        return tf.reduce_mean(tf.square(content_image - target_image))
+        return PIL.Image.fromarray(tensor)
 
-    def _compute_style_loss(self, style_features, target_features):
-        """Compute the style loss."""
-        style_loss = 0
-        for style, target in zip(style_features, target_features):
-            style_loss += tf.reduce_mean(tf.square(self._gram_matrix(style) - self._gram_matrix(target)))
-        return style_loss
+    def transfer_style(self):
+        content_image = self.load_image(self.content_image_path)
+        style_image = self.load_image(self.style_image_path)
 
-    def _compute_total_variation_loss(self, image):
-        """Compute total variation loss for smoothness."""
-        x_variation = image[:, 1:, :, :] - image[:, :-1, :, :]
-        y_variation = image[:, :, 1:, :] - image[:, :, :-1, :]
-        return tf.reduce_sum(tf.abs(x_variation)) + tf.reduce_sum(tf.abs(y_variation))
+        stylized_image = self.model(content_image, style_image)[0]
 
-    def _gram_matrix(self, input_tensor):
-        """Compute the Gram matrix for a given tensor."""
-        result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
-        return result / tf.cast(input_tensor.shape[1] * input_tensor.shape[2], tf.float32)
-
-
-    def _total_loss(self, content_features, style_features, target_content_features, target_style_features, target_image):
-        """Compute the total loss (content + style + total variation)."""
-        content_loss = self._compute_content_loss(content_features, target_content_features)
-        style_loss = self._compute_style_loss(style_features, target_style_features)
-        total_variation_loss = self._compute_total_variation_loss(target_image)
-        
-        total_loss = self.content_weight * content_loss + self.style_weight * style_loss + self.total_variation_weight * total_variation_loss
-        return total_loss
-
-
-    def transfer_style(self, content_image_path, style_image_path, num_iterations=100, learning_rate=5.0):
-        """Perform style transfer."""
-        content_image = self._preprocess_image(content_image_path)
-        style_image = self._preprocess_image(style_image_path)
-
-        target_image = tf.Variable(content_image)
-
-        optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
-
-        with tqdm(range(num_iterations), desc="Style Transfer Progress", unit="iteration") as pbar:
-            for iteration in pbar:
-                with tf.GradientTape() as tape:
-                    tape.watch(target_image)
-                    
-                    content_features, style_features = self._get_content_and_style_features(content_image, style_image)
-                    target_content_features, target_style_features = self._get_content_and_style_features(target_image, style_image)
-
-                    
-                    loss = self._total_loss(content_features, style_features, target_content_features, target_style_features, target_image)
-                
-                gradients = tape.gradient(loss, target_image)
-                gradients = tf.clip_by_value(gradients, -1.0, 1.0)
-                optimizer.apply_gradients([(gradients, target_image)])
-
-                pbar.set_postfix({"loss": loss.numpy()})
-
-        return self._deprocess_image(target_image)
+        return self.tensor_to_image(stylized_image)
--- styletransfer/views.py
+++ styletransfer/views.py
@@ -4,8 +4,8 @@ from django.utils.timezone import now
 import os
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-
 from .style_transfer import StyleTransfer
+import tensorflow as tf
 
 @csrf_exempt
 def style_transfer_view(request):
@@ -13,23 +13,29 @@ def style_transfer_view(request):
         content_image = request.FILES['content_image']
         style_image = request.FILES['style_image']
 
+        if content_image.size == 0 or style_image.size == 0:
+            return JsonResponse({'error': 'One or both uploaded images are empty.'}, status=400)
+
+        if 'image' not in content_image.content_type or 'image' not in style_image.content_type:
+            return JsonResponse({'error': 'Please upload valid image files.'}, status=400)
+
         folder_name = now().strftime("%Y-%m-%d_%H-%M-%S")
         media_path = os.path.join(settings.MEDIA_ROOT, folder_name)
         os.makedirs(media_path, exist_ok=True)
 
         content_image_path = os.path.join(media_path, 'content_image.png')
         style_image_path = os.path.join(media_path, 'style_image.png')
-        
+
         fs = FileSystemStorage(location=media_path)
         fs.save('content_image.png', content_image)
         fs.save('style_image.png', style_image)
 
-        style_transfer = StyleTransfer(content_weight=1e3, style_weight=1e2)
+        style_transfer = StyleTransfer(content_image_path, style_image_path)
 
-        result_image = style_transfer.transfer_style(content_image_path, style_image_path, num_iterations=50, learning_rate=1e-2)
+        stylized_image = style_transfer.transfer_style()
 
         result_image_path = os.path.join(media_path, 'style_transferred_image.png')
-        result_image.save(result_image_path)
+        stylized_image.save(result_image_path)
 
         return JsonResponse({
             'content_image': content_image_path,
