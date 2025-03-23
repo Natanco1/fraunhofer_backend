ff5e60028a9c53f937f7c07c64091352fcdd97ee
Natanco1 <nata.oliveira2004@gmail.com>
2025-03-23 10:28:14 -0300

    feat: update settings

--- style_transfer_backend/settings.py
+++ style_transfer_backend/settings.py
@@ -124,5 +124,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
